d3e1fc0d68f2f166051718077c6a28c0bdf87084
dest1yo <hiangu@qq.com>
2025-06-01 17:00:35 +0800

    ??

--- casclib-sys/build.rs
+++ casclib-sys/build.rs
@@ -1,6 +1,24 @@
 extern crate cmake;
 
 use std::{env, fs};
+use std::{process::Command};
+
+// Return (major, minor)
+fn get_cmake_version() -> Option<(u32, u32, u32)> {
+    let output = Command::new("cmake").arg("--version").output().ok()?;
+    if !output.status.success() {
+        return None;
+    }
+    let stdout = core::str::from_utf8(&output.stdout).ok()?;
+
+    // Parse
+    let version = stdout.lines().next()?.strip_prefix("cmake version ")?;
+    let mut digits = version.splitn(3, '.'); // split version string to major minor patch
+    let major = digits.next()?.parse::<u32>().ok()?;
+    let minor = digits.next()?.parse::<u32>().ok()?;
+    let patch = digits.next()?.parse::<u32>().ok()?;
+    Some((major, minor, patch))
+}
 
 fn main() {
     // Gets CacsLib source path from env CASCLIB_DIR
@@ -10,12 +28,18 @@ fn main() {
 
     let mut cfg = cmake::Config::new(&casclib_path);
 
+    // TODO: CMake requires higher minimum version in new release, and CascLib has not been updated for it yet
+    let cmake_version = get_cmake_version().unwrap_or_default();
+    if cmake_version >= (4, 0, 0) {
+        println!("CMake version: {:?} is >= 4.0, option \"-CMAKE_POLICY_VERSION_MINIMUM=3.5\" is required", cmake_version);
+        cfg.define("CMAKE_POLICY_VERSION_MINIMUM", "3.5");
+    }
+
     #[cfg(target_os = "windows")]
     {
         cfg.cxxflag("-D UNICODE")
             .cxxflag("-D _UNICODE")
-            .cxxflag("-D CASC_UNICODE")
-            .cxxflag("-D CASCLIB_UNICODE");
+            .define("CASC_UNICODE", "ON");
     }
 
     // Builds CascLib using cmake
